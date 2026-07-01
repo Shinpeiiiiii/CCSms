3d44e36c3bcdc6522da7c96b70f97d476d6a27a8
danshi <seddy012345@gmail.com>
2026-07-01 19:17:29 +0800

    change some env variable of the frontend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import useAuthStore from '../modules/auth/state/auth-store';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type": "application/json",
     },
