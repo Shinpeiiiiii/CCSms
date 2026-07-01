aae25895f9c8b03ec596c81444ff9feb77993f62
danshi <seddy012345@gmail.com>
2026-07-01 19:08:42 +0800

    add env variables of the client

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import useAuthStore from '../modules/auth/state/auth-store';
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
         "Content-Type": "application/json",
     },
