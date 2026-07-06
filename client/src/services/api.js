c74c48a085a5f0f8e9856d6ae97fe522e83e9ce3
danshi <seddy012345@gmail.com>
2026-07-06 17:18:48 +0800

    fix base url deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import useAuthStore from '../modules/auth/state/auth-store';
 
 const api = axios.create({
-    baseURL: "/api",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
     timeout: 10000,
     headers: {
